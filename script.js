902960ccf617af76f0e0ffacd1525f418250b49f
komalk-prog <komalkushwaha172@gmail.com>
2023-02-06 20:39:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+async function munction() {
+  var response = await fetch("https://animechan.vercel.app/api/random");
+  var data = await response.json();
+
+  let aName = document.getElementById("name");
+  let aQuote = document.getElementById("quote");
+  let aChar = document.getElementById("char");
+
+  aName.innerHTML = data.anime;
+  aQuote.innerHTML = data.quote;
+  aChar.innerHTML = "Sneha kumari";
+}
+munction();
