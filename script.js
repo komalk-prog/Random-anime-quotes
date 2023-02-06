7987caa9428f7b2e0e87a62d9bf80c1220ea0a07
komalk-prog <komalkushwaha172@gmail.com>
2023-02-06 21:00:50 +0530

    fixed src

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 async function munction() {
+  console.log("start");
   var response = await fetch("https://animechan.vercel.app/api/random");
   var data = await response.json();
-
+  console.log(data);
   let aName = document.getElementById("name");
   let aQuote = document.getElementById("quote");
   let aChar = document.getElementById("char");
